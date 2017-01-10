a3127a144c468e3ef113c12392a7978c33d9ce66
MeteorSis <uncleone@naver.com>
2017-01-10 11:46:48 +0900

    Hi World!!!

---
+++ app/src/main/java/cse/mobile/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package cse.mobile.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv1=(TextView)findViewById(R.id.tv1);
+        tv1.setText("Hi World!");
+    }
+}
